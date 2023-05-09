3a7be2f562f4e59346bf0fca8e0494e04ec3cc65
d8ahazard <d8ahazard@gmail.com>
2023-05-09 10:09:25 -0500

    Update diffusers_loader.py

--- core/handlers/model_types/diffusers_loader.py
+++ core/handlers/model_types/diffusers_loader.py
@@ -177,75 +177,68 @@ def load_diffusers_controlnet(model_data: ModelData):
     return pipeline
 
 
-def apply_lora(pipeline: DiffusionPipeline, lora_path: str):
-    if not os.path.exists(lora_path):
-        logger.debug(f"Unable to load lora: {lora_path}")
-        return
-    state_dict = load_file(lora_path)
-
-    LORA_PREFIX_UNET = 'lora_unet'
-    LORA_PREFIX_TEXT_ENCODER = 'lora_te'
-
-    alpha = 0.75
+def apply_lora(pipeline, checkpoint_path, alpha=0.75):
+    lora_prefix_unet = "lora_unet"
+    lora_prefix_text_encoder = "lora_te"
+    # load LoRA weight from .safetensors
+    state_dict = load_file(checkpoint_path)
 
     visited = []
 
     # directly update weight in diffusers model
     for key in state_dict:
-        try:
-            # it is suggested to print out the key, it usually will be something like below
-            # "lora_te_text_model_encoder_layers_0_self_attn_k_proj.lora_down.weight"
+        # it is suggested to print out the key, it usually will be something like below
+        # "lora_te_text_model_encoder_layers_0_self_attn_k_proj.lora_down.weight"
 
-            # as we have set the alpha beforehand, so just skip
-            if '.alpha' in key or key in visited:
-                continue
+        # as we have set the alpha beforehand, so just skip
+        if ".alpha" in key or key in visited:
+            continue
 
-            if 'text' in key:
-                layer_infos = key.split('.')[0].split(LORA_PREFIX_TEXT_ENCODER + '_')[-1].split('_')
-                curr_layer = pipeline.text_encoder
-            else:
-                layer_infos = key.split('.')[0].split(LORA_PREFIX_UNET + '_')[-1].split('_')
-                curr_layer = pipeline.unet
+        if "text" in key:
+            layer_infos = key.split(".")[0].split(lora_prefix_text_encoder + "_")[-1].split("_")
+            curr_layer = pipeline.text_encoder
+        else:
+            layer_infos = key.split(".")[0].split(lora_prefix_unet + "_")[-1].split("_")
+            curr_layer = pipeline.unet
 
-            # find the target layer
-            temp_name = layer_infos.pop(0)
-            while len(layer_infos) > -1:
-                try:
-                    curr_layer = curr_layer.__getattr__(temp_name)
-                    if len(layer_infos) > 0:
-                        temp_name = layer_infos.pop(0)
-                    elif len(layer_infos) == 0:
-                        break
-                except Exception:
-                    if len(temp_name) > 0:
-                        temp_name += '_' + layer_infos.pop(0)
-                    else:
-                        temp_name = layer_infos.pop(0)
+        # find the target layer
+        temp_name = layer_infos.pop(0)
+        while len(layer_infos) > -1:
+            try:
+                curr_layer = curr_layer.__getattr__(temp_name)
+                if len(layer_infos) > 0:
+                    temp_name = layer_infos.pop(0)
+                elif len(layer_infos) == 0:
+                    break
+            except Exception:
+                if len(temp_name) > 0:
+                    temp_name += "_" + layer_infos.pop(0)
+                else:
+                    temp_name = layer_infos.pop(0)
 
-            # org_forward(x) + lora_up(lora_down(x)) * multiplier
-            pair_keys = []
-            if 'lora_down' in key:
-                pair_keys.append(key.replace('lora_down', 'lora_up'))
-                pair_keys.append(key)
-            else:
-                pair_keys.append(key)
-                pair_keys.append(key.replace('lora_up', 'lora_down'))
+        pair_keys = []
+        if "lora_down" in key:
+            pair_keys.append(key.replace("lora_down", "lora_up"))
+            pair_keys.append(key)
+        else:
+            pair_keys.append(key)
+            pair_keys.append(key.replace("lora_up", "lora_down"))
 
-            # update weight
-            if len(state_dict[pair_keys[0]].shape) == 4:
-                weight_up = state_dict[pair_keys[0]].squeeze(3).squeeze(2).to(torch.float32)
-                weight_down = state_dict[pair_keys[1]].squeeze(3).squeeze(2).to(torch.float32)
-                curr_layer.weight.data += alpha * torch.mm(weight_up, weight_down).unsqueeze(2).unsqueeze(3)
-            else:
-                weight_up = state_dict[pair_keys[0]].to(torch.float32)
-                weight_down = state_dict[pair_keys[1]].to(torch.float32)
-                curr_layer.weight.data += alpha * torch.mm(weight_up, weight_down)
+        # update weight
+        if len(state_dict[pair_keys[0]].shape) == 4:
+            weight_up = state_dict[pair_keys[0]].squeeze(3).squeeze(2).to(torch.float32)
+            weight_down = state_dict[pair_keys[1]].squeeze(3).squeeze(2).to(torch.float32)
+            curr_layer.weight.data += alpha * torch.mm(weight_up, weight_down).unsqueeze(2).unsqueeze(3)
+        else:
+            weight_up = state_dict[pair_keys[0]].to(torch.float32)
+            weight_down = state_dict[pair_keys[1]].to(torch.float32)
+            curr_layer.weight.data += alpha * torch.mm(weight_up, weight_down)
 
-            # update visited list
-            for item in pair_keys:
-                visited.append(item)
-        except Exception as e:
-            logger.warning(f"Exception loading lora key {key}: {e}")
+        # update visited list
+        for item in pair_keys:
+            visited.append(item)
+
+    return pipeline
 
 
 def register_function(model_handler):
